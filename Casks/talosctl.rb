6e35509cc40b4c57b8af34160d42fc72ae001bf7
Serge Logvinov <serge.logvinov@sinextra.dev>
2022-03-31 10:34:42 +0300

    chore: change the repository url
    SiredoLabs renamed the github repository.
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>

--- Casks/talosctl.rb
+++ Casks/talosctl.rb
@@ -5,13 +5,13 @@ cask "talosctl" do
 
   if Hardware::CPU.intel?
     sha256 "cc460ba5347f11b1baa793729a9d475311c525f29be0edeb3d486816b4e15f9a"
-    url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-amd64",
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-amd64",
       verified: "github.com/talos-systems/talos/"
 
     binary "talosctl-darwin-amd64", target: "talosctl"
   else
     sha256 "ac62572ae5cd71a02ccbb91ef14edc4970a468f4595adeca93e66dea74b03bbd"
-    url "https://github.com/talos-systems/talos/releases/download/v#{version}/talosctl-darwin-arm64",
+    url "https://github.com/siderolabs/talos/releases/download/v#{version}/talosctl-darwin-arm64",
       verified: "github.com/talos-systems/talos/"
 
     binary "talosctl-darwin-arm64", target: "talosctl"
